1d8f5f6ddcc2adb230428c426aad3daa9ab94d1d
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2026-02-25 02:17:10 +0700

    praktikum2_no11

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/', [HomeController::class,'index']);
 Route::get('/about', [AboutController::class,'about']);
 Route::get('/articles/{id}', [ArticleController::class,'articles']);
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
